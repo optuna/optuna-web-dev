83485b79709f4d4f154fa34c0971116393eb0512
DmitriyShvanyk <soledar1984@gmail.com>
2020-04-10 12:32:41 +0300

    Add svg sprites, fix js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -39,4 +39,43 @@
     $('.nav-tabs a[href="#' + tabName + '"]').tab('show');
   }
 
+
+  // navbar
+  $(document).on('click', '.navbar-toggler', function () {
+    $toggle = $(this);
+    if (pk.misc.navbar_menu_visible == 1) {
+      $('html').removeClass('nav-open');
+      pk.misc.navbar_menu_visible = 0;
+      setTimeout(function () {
+        $toggle.removeClass('toggled');
+        $('#bodyClick').remove();
+      }, 550);
+    } else {
+      setTimeout(function () {
+        $toggle.addClass('toggled');
+      }, 580);
+
+      div = '<div id="bodyClick"></div>';
+      $(div).appendTo("body").click(function () {
+        $('html').removeClass('nav-open');
+        pk.misc.navbar_menu_visible = 0;
+        $('#bodyClick').remove();
+        setTimeout(function () {
+          $toggle.removeClass('toggled');
+        }, 550);
+      });
+
+      $('html').addClass('nav-open');
+      pk.misc.navbar_menu_visible = 1;
+    }
+  }); 
+
+
+
+  $('.profile-tabs .nav-link').on('click', function(){
+    setTimeout(function () {
+      bLazy.revalidate();
+    }, 100);
+  });
+
 })();
