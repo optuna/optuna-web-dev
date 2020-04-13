e2c25df03e255b823b94a83be37df109fbb88e65
DmitriyShvanyk <soledar1984@gmail.com>
2020-04-13 14:01:48 +0300

    Add lazy load twitter widget

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,8 +11,7 @@
     head.appendChild(link);
   }
   loadFonts('https://fonts.googleapis.com/css?family=Montserrat:400,300,700|Work+Sans|Material+Icons&display=swap');
-  //loadFonts('https://use.fontawesome.com/releases/v5.3.1/css/all.css');
-
+  
 
   // current years
   function setCurrentYears(domElement) {
@@ -45,5 +44,49 @@
       bLazy.revalidate();
     }, 100);
   });
+
+
+  // load twitter widget
+  function loadPlatformTwitter() {
+    let body = document.querySelector('body');
+    let script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://platform.twitter.com/widgets.js';
+    body.appendChild(script);
+  }  
+
+  // Add Class to Element when Scrolled into View
+  function hasClass(el, cls) {
+    if (el.className.match('(?:^|\\s)' + cls + '(?!\\S)')) { return true; }
+  }
+  function addClass(el, cls) {
+    if (!el.className.match('(?:^|\\s)' + cls + '(?!\\S)')) { el.className += ' ' + cls; }
+  }
+  function delClass(el, cls) {
+    el.className = el.className.replace(new RegExp('(?:^|\\s)' + cls + '(?!\\S)'), '');
+  }
+
+  function elementFromTop(elem, classToAdd, distanceFromTop, unit) {
+    var winY = window.innerHeight || document.documentElement.clientHeight,
+      distTop = elem.getBoundingClientRect().top,
+      distPercent = Math.round((distTop / winY) * 100),
+      distPixels = Math.round(distTop),
+      distUnit;
+    distUnit = unit == 'percent' ? distPercent : distPixels;
+    if (distUnit <= distanceFromTop) {
+      if (!hasClass(elem, classToAdd)) { addClass(elem, classToAdd); }
+    } else {
+      delClass(elem, classToAdd);
+    }
+  }
+
+  window.addEventListener('scroll', function () {
+    elementFromTop(document.querySelector('.news'), 'news--widget-on', 200, 'pixels');
+    
+    setTimeout(() => { 
+      loadPlatformTwitter(); 
+    }, 1000);
+
+  }, false);
 
 })();
